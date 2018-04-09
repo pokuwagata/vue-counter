b78bc743c4b9d89d9384c950519e583fdd307996
pokuwagata <pokuwagata@gmail.com>
2018-04-10 00:19:14 +0900

    create counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{count}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data: function() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment: function() {
+      this.count++
+    },
+    decrement: function() {
+      this.count--
+    }
+  }
+}
+</script>
+
